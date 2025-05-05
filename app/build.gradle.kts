01c2df2f499c12237df733626cd1e717439dfcfd
AzureUITGithub <hathean2018@gmail.com>
2025-05-05 15:13:53 +0700

    config retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,5 +42,7 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.activity)
+    implementation(libs.retrofit)
+    implementation(libs.retrofitGson)
 }
 
