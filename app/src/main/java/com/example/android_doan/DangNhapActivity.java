fe3a56c0c8b5588a6d494d3008823cd71cbb7d29
MinHeHeHeHe <minhhytra@gmail.com>
2025-05-05 12:01:15 +0700

    Them màn hình quên mật khẩu

--- app/src/main/java/com/example/android_doan/DangNhapActivity.java
+++ app/src/main/java/com/example/android_doan/DangNhapActivity.java
@@ -7,6 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.widget.TextView;
+import android.graphics.Typeface;
+import android.view.MotionEvent;
+import android.view.View;
+
 
 public class DangNhapActivity extends AppCompatActivity {
 
@@ -20,5 +26,46 @@ public class DangNhapActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Gắn sự kiện click cho TextView "Đăng ký ngay"
+        TextView tvDangKy = findViewById(R.id.text_chua_co_tai_khoan);
+        tvDangKy.setOnClickListener(v -> {
+            Intent intent = new Intent(DangNhapActivity.this, DangKyActivity.class);
+            startActivity(intent);
+        });
+
+        tvDangKy.setOnTouchListener((v, event) -> {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    tvDangKy.setTypeface(null, Typeface.BOLD);
+                    break;
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    tvDangKy.setTypeface(null, Typeface.NORMAL);
+                    break;
+            }
+            return false;
+        });
+
+        // Gắn sự kiện click cho TextView "Quên mật khẩu"
+        TextView tvQuenMatKhau = findViewById(R.id.txtQuenMatKhau);
+        tvQuenMatKhau.setOnClickListener(v -> {
+            Intent intent = new Intent(DangNhapActivity.this, QuenMatKhauActivity.class);
+            startActivity(intent);
+        });
+
+        tvQuenMatKhau.setOnTouchListener((v, event) -> {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    tvQuenMatKhau.setTypeface(null, Typeface.BOLD);
+                    break;
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    tvQuenMatKhau.setTypeface(null, Typeface.NORMAL);
+                    break;
+            }
+            return false;
+        });
+
     }
 }
