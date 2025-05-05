20c5403a56afdea1ea68b8b0405499cf1c9ff188
MinHeHeHeHe <minhhytra@gmail.com>
2025-05-05 09:29:25 +0700

    Sua loi 05/05 lỗi quên gọi string với tên mới

--- app/src/main/java/com/example/android_doan/HoSoNguoiDungActivity.java
+++ app/src/main/java/com/example/android_doan/HoSoNguoiDungActivity.java
@@ -27,7 +27,7 @@ public class HoSoNguoiDungActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_map, R.id.navigation_orders, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_ho_so_nguoi_dung);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
