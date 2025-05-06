3e6511f74fcac739c20480baff520eb0171ebe2b
MinHeHeHeHe <minhhytra@gmail.com>
2025-05-06 15:35:28 +0700

    xong giao dien giua ky

--- app/src/main/java/com/example/android_doan/ui/profile/ChinhSuaThongTinActivity.java
+++ app/src/main/java/com/example/android_doan/ui/profile/ChinhSuaThongTinActivity.java
@@ -1,6 +1,7 @@
 package com.example.android_doan.ui.profile;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +23,12 @@ public class ChinhSuaThongTinActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Gắn sự kiện cho nút quay lại
+        ImageView imgBack = findViewById(R.id.img_chevron_left);
+        imgBack.setOnClickListener(v -> {
+            finish(); // Quay về fragment trước đó
         });
     }
 }
