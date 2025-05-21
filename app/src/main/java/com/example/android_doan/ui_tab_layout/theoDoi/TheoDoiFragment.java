81ac2b1c4552942b2281a1f41c8ab7a6949a105e
MinHeHeHeHe <minhhytra@gmail.com>
2025-05-21 10:31:21 +0700

    Them phan recycler view cho home fragment

---
+++ app/src/main/java/com/example/android_doan/ui_tab_layout/theoDoi/TheoDoiFragment.java
@@ -0,0 +1,33 @@
+package com.example.android_doan.ui_tab_layout.theoDoi;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.android_doan.databinding.FragmentThanhToanBinding;
+
+public class TheoDoiFragment extends Fragment {
+
+    private FragmentThanhToanBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentThanhToanBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
+
